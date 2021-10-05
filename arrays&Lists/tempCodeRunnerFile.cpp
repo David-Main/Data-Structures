443e8c700071af506f18a3c8c73959bb5adec448
Nyameky3 <kwabenanyameky3@gmail.com>
2021-10-05 04:13:13 +0000

    finished implementing all functions of linked list

--- arrays&Lists/tempCodeRunnerFile.cpp
+++ arrays&Lists/tempCodeRunnerFile.cpp
@@ -1,39 +1,135 @@
-list();
+#include <iostream>
+#include <iomanip>
 
-	// list(int value){
-	// 	list temp;
-	// 	temp.data = value;
-	// 	temp.next = nullptr;
-	// 	listCount++;
-	// }
+using namespace std;
 
-	// list * searchList (int value)
-	// {
-	// 	list *temp = this;
-	// 	while(temp->data != value)
-	// 	{
-	// 		if(temp == nullptr) return nullptr;		//return nullptr if item is not found.
-	// 		temp = temp->next;
-	// 	}
-	// 	return temp;
-	// }
 
-	// void add(int value)
-	// {
-	// 	list *temp = this->next;
-	// 	while(temp->next != nullptr)
-	// 	{
-	// 		temp = temp->next;
-	// 	}
-	// 	temp->next = new(list);
-	// 	temp->data = value;
-	// 	listCount += 1;
-	// }
+class list {
 
-	// bool empty(){
-	// 	return (0 == listCount ? true : false);		//return true if empty or false otherwise.
-	// }
+	private:
+		struct node {
+			int data;
+			node* next = nullptr;
+		};
 
-	// int getCount(){
-	// 	return listCount;
-	// }
+
+	public:	//variables
+		node* head = nullptr;
+		size_t listCount = 0;
+
+
+	public: //functions
+		list()
+		{
+			listCount = 0;
+		}
+
+		list(int value)
+		{
+			this->head = new(node);
+			this->head->data = value;
+			listCount++;
+		}
+
+		void add(int value)
+		{
+			auto temp = head;				// get head of list
+			if(nullptr == temp)
+			{
+				temp = new node{value};
+				return;
+			}
+			while(temp->next != nullptr)	// go to last node of list
+			{
+				temp = temp->next;
+			}
+			temp->next = new node;			// append new node to last node
+			temp = temp->next;				// move to new node
+			
+			temp->data = value;				// add value.
+			listCount++;
+		}
+
+		void print()
+		{
+			node* temp = head;
+			while(temp != nullptr)
+			{	
+				cout << setw(3) << setfill(' ') << temp->data;
+				temp = temp->next;
+			}
+			cout << endl;
+		}
+
+		bool insert(int value, size_t position)
+		{
+			if(position > listCount) 
+				return false;						//position not in range
+
+			node* currentNode = head;
+			node* prev;
+			size_t currentPosition = 1;
+
+			while(currentPosition != position)		//go to node in question
+			{
+				prev = currentNode;
+				currentNode = currentNode->next;
+				currentPosition++;
+			}
+			
+			if(currentNode == head)
+			{
+				//create new node with given value,
+				//set newNode.next = head
+			    //set head to newNode;
+				head = new node{value, head};
+			}
+			else
+			{
+				// Create new node with given value as data and currentNode as next;
+				// Set prev->next to new Node;
+				prev->next = new node{value, currentNode};
+
+			}
+			listCount++;
+			return true; 							//insert complete
+		}
+
+		size_t search(int value)
+		{
+			node* currentNode = head;
+			size_t currentPosition = 1;
+
+			while(currentNode->data != value && currentNode != nullptr)		//go to node in question
+			{
+				if(currentNode->next == nullptr)
+				{
+					currentPosition = 0;
+					return currentPosition;
+				}
+				currentNode = currentNode->next;
+				currentPosition++;
+			}
+			return currentPosition;
+		}
+};
+
+int main()
+{
+	list items(2);
+	items.add(4);
+	items.add(5);
+
+	items.insert(8, 2);
+	items.insert(19, 1);
+
+	items.print();
+	cout << "List Count: " << items.listCount << endl;
+	cout << "Found at position: " << items.search(23) << endl;
+
+	list materials;
+	cout << materials.listCount;
+	// // // materials.add(2);
+	
+	return 0;
+}
