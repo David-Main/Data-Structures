1e953557757ca6f93e0a0a3bc37fbeb51940375f
Nyameky3 <kwabenanyameky3@gmail.com>
2021-10-08 01:46:10 +0000

    Added a few other functions
    Added insert function, search function and overloaded print
    function so it can print specific positions of the list.

--- arrays&Lists/Implementation/doublyLinkedLists.cpp
+++ arrays&Lists/Implementation/doublyLinkedLists.cpp
@@ -29,10 +29,15 @@ public: //functions
 		++listCount;
 	}
 
+	bool isEmpty()
+	{
+		return ((0 == listCount) ? true : false);
+	}
+
 	void add(int value)
 	{
 		node *temp = headNode;
-		if (0 == listCount && nullptr == headNode)
+		if (isEmpty() && nullptr == headNode)
 		{
 			headNode = new node{value};
 			++listCount;
@@ -51,17 +56,74 @@ public: //functions
 	void print()
 	{
 		node *temp = headNode;
-		if (0 == listCount)
-		{
-			cout << "list is empty\n";
+		if (isEmpty())
 			return;
-		}
-
 		while (temp != nullptr)
 		{
 			cout << setw(4) << temp->data;
 			temp = temp->next;
 		}
+		cout << endl;
+	}
+
+	void print(size_t position)
+	{
+		if (isEmpty() || (position > listCount))
+			return;
+
+		node *temp = headNode;
+		size_t currentPos = 1;
+		while (currentPos != position)
+		{
+			temp = temp->next;
+			++currentPos;
+		}
+		cout << temp->data << endl;
+	}
+
+	void insert(int value, size_t position)
+	{
+		if (isEmpty())
+			return;
+		if (position > listCount)
+		{
+			cout << "Position out of range:" << endl;
+			return;
+		}
+		node *currentNode = headNode;
+		size_t currentPos = 1;
+		while (currentPos != position)
+		{
+			currentNode = currentNode->next;
+			++currentPos;
+		}
+		if (1 == currentPos && currentNode == headNode)
+		{
+			currentNode->prev = new node{value, nullptr, currentNode};
+			headNode = currentNode->prev;
+		}
+		else
+		{
+			currentNode->prev->next = new node{value, currentNode->prev, currentNode};
+			currentNode->prev = currentNode->prev->next;
+		}
+		++listCount;
+	}
+
+	size_t search(int value) //returns 0 if item is not found
+	{
+		if (!isEmpty())
+		{
+			node *temp = headNode;
+			size_t position = 1;
+			while ((value != temp->data) && (nullptr != temp->next))
+			{
+				temp = temp->next;
+				++position;
+			}
+			return ((temp->data == value) ? (position) : (0));
+		}
+		return 0;
 	}
 };
 
@@ -71,6 +133,21 @@ int main()
 	items.add(3);
 
 	items.print();
+
+	//Insert Test
+	for (int i = 1, p = 1; i < 30; i *= 2, ++p)
+	{
+		items.insert(i, p);
+	}
+	items.print();
+
+	items.search(32);
+
+	//Overloaded Print test
+	for (int i = 1; i <= items.listCount; ++i)
+	{
+		items.print(i);
+	}
 
 	return 0;
 }
